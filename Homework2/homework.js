c6062f9e61765f6897fb7ed0730321da584fdfff
Z1llya <zubalii.illya@gmail.com>
2022-04-19 10:33:40 +0300

    inittial

---
+++ Homework2/homework.js
@@ -0,0 +1,82 @@
+// part1
+
+// let a = 2;
+// if ( a !== 0 ){
+//     console.log('вірно');
+//
+// }else {
+//     console.log('невірно');
+// }
+
+// part2
+
+// let time=45;
+//
+// if (time<15){
+//     console.log("1 чверть");
+// }
+// else if (time>=15 && time<30){
+//     console.log("2 чверть");
+// }
+// else if(time>=30 && time<45){
+//     console.log("3 чверть");
+// }
+// else if(time>=45 && time<60){
+//     console.log("4 чверть");
+// }
+//
+
+// part3
+
+// let day= 20;
+// if(day<11){
+//     console.log("first decade")
+// }
+// else if(day>10 && day<21 ){
+//     console.log("second decade")
+// }
+// else if(day>20 && day<32){
+//     console.log("fird decade")
+// }
+
+// part4
+
+// let day = prompt("введіть номер дня тижня")
+//
+// switch (day){
+//     case "1":
+//         document.write('<div>Lorem</div>');
+//         break;
+//         case "2":
+//         document.write('<div>Lorem ipsum </div>');
+//         break;
+//         case "3":
+//         document.write('<div>Lorem ipsum dolor </div>');
+//         break;
+//         case "4":
+//         document.write('<div>Lorem ipsum dolor sit </div>');
+//         break;
+//         case "5":
+//         document.write('<div>Lorem ipsum dolor sit amet.</div>');
+//         break;
+//         case "6":
+//         document.write('<div>Lorem ipsum dolor sit amet, consectetur./div>');
+//         break;
+//         case "7":
+//         document.write('<div>Lorem ipsum dolor sit amet, consectetur adipisicing.</div>');
+//         break;
+// }
+
+// part5
+
+// let a = prompt("enter a first number");
+// let b = prompt("enter a second number");
+//
+// if (a>b){
+//     document.write('<div>first is bigger than second</div>');
+// }
+// else if(a<b){
+//     document.write('<div>second is bigger than first</div>');
+// }else if(a===b){
+//     document.write('<div>first is equal to second</div>');
+// }
